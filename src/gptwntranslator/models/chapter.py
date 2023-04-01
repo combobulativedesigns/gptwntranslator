d9b1e3187b43da7cf389410f07532fa249395cea
Rodrigo S. Jauregui <rodrigo.jauregui@correo.ucu.edu.uy>
2023-04-01 00:14:31 -0300

    Tests for chapter.py

--- src/gptwntranslator/models/chapter.py
+++ src/gptwntranslator/models/chapter.py
@@ -1,15 +1,29 @@
+"""Chapter model"""
+
 from gptwntranslator.models.sub_chapter import SubChapter
 
 
 class Chapter:
-    def __init__(self, chapter_index, name, translated_name, sub_chapters):
+    """Chapter model"""
+    
+    def __init__(self, chapter_index: int, name: str, translated_name: str, sub_chapters: list[SubChapter]) -> None:
+        """
+        Initialize a Chapter object.
+
+        Arguments:
+            chapter_index (int) - The index of the chapter.
+            name (str) - The name of the chapter.
+            translated_name (str) - The translated name of the chapter.
+            sub_chapters (list[SubChapter]) - The list of sub-chapters in the chapter.
+        """
+
         if not isinstance(chapter_index, int):
             raise TypeError("Index must be an integer")
         if not isinstance(name, str):
             raise TypeError("Name must be a string")
         if not isinstance(translated_name, str):
             raise TypeError("Translated name must be a string")
-        if not isinstance(sub_chapters, list):
+        if not isinstance(sub_chapters, list[SubChapter]):
             raise TypeError("Sub-chapters must be a list of SubChapter objects")
         
         self.chapter_index = chapter_index
@@ -17,38 +31,84 @@ class Chapter:
         self.translated_name = translated_name
         self.sub_chapters = sub_chapters
 
-    def __str__(self):
+    def __str__(self) -> str:
+        """Return the string representation of a Chapter object."""
+
         return f"Chapter {self.chapter_index}"
     
-    def __repr__(self):
+    def __repr__(self) -> str:
+        """Return the string representation of a Chapter object."""
+
         return f"Chapter {self.chapter_index}"
     
-    def __eq__(self, other):
+    def __eq__(self, other) -> bool:
+        """
+        Return True if the two Chapter objects are equal.
+        
+        Arguments:
+            other (Chapter) - The other Chapter object to compare.
+        """	
+
         if not isinstance(other, Chapter):
             raise TypeError("Other object must be a Chapter object")
         return self.chapter_index == other.chapter_index
     
-    def __ne__(self, other):
+    def __ne__(self, other) -> bool:
+        """
+        Return True if the two Chapter objects are not equal.
+        
+        Arguments:
+            other (Chapter) - The other Chapter object to compare.
+        """
+
         if not isinstance(other, Chapter):
             raise TypeError("Other object must be a Chapter object")
         return self.chapter_index != other.chapter_index
     
-    def __lt__(self, other):
+    def __lt__(self, other) -> bool:
+        """
+        Return True if the first Chapter object is less than the second.
+        
+        Arguments:
+            other (Chapter) - The other Chapter object to compare.
+        """
+
         if not isinstance(other, Chapter):
             raise TypeError("Other object must be a Chapter object")
         return self.chapter_index < other.chapter_index
     
-    def __le__(self, other):
+    def __le__(self, other) -> bool:
+        """
+        Return True if the first Chapter object is less than or equal to the second.
+        
+        Arguments:
+            other (Chapter) - The other Chapter object to compare.
+        """
+
         if not isinstance(other, Chapter):
             raise TypeError("Other object must be a Chapter object")
         return self.chapter_index <= other.chapter_index
     
-    def __gt__(self, other):
+    def __gt__(self, other) -> bool:
+        """
+        Return True if the first Chapter object is greater than the second.
+        
+        Arguments:
+            other (Chapter) - The other Chapter object to compare.
+        """
+
         if not isinstance(other, Chapter):
             raise TypeError("Other object must be a Chapter object")
         return self.chapter_index > other.chapter_index
     
-    def __ge__(self, other):
+    def __ge__(self, other) -> bool:
+        """
+        Return True if the first Chapter object is greater than or equal to the second.
+        
+        Arguments:
+            other (Chapter) - The other Chapter object to compare.
+        """
+
         if not isinstance(other, Chapter):
             raise TypeError("Other object must be a Chapter object")
         return self.chapter_index >= other.chapter_index
---
+++ tests/tests_chapter.py
@@ -0,0 +1,80 @@
+"""Tests for the Chapter class."""
+
+import unittest
+from gptwntranslator.models.chapter import Chapter
+
+
+class TestChapter(unittest.TestCase):
+    """Tests for the Chapter class."""
+
+    def test_init(self) -> None:
+        """Test that the Chapter class is initialized correctly."""
+        chapter = Chapter(1, "Introduction", "Introducción", [])
+        self.assertEqual(chapter.chapter_index, 1)
+        self.assertEqual(chapter.name, "Introduction")
+        self.assertEqual(chapter.translated_name, "Introducción")
+        self.assertEqual(chapter.sub_chapters, [])
+
+    def test_init_invalid_arguments(self) -> None:
+        """Test that the Chapter class raises the correct exceptions."""
+        with self.assertRaises(TypeError):
+            Chapter("1", "Introduction", "Introducción", [])
+
+        with self.assertRaises(TypeError):
+            Chapter(1, 1, "Introducción", [])
+
+        with self.assertRaises(TypeError):
+            Chapter(1, "Introduction", 1, [])
+
+        with self.assertRaises(TypeError):
+            Chapter(1, "Introduction", "Introducción", "[]")
+
+    def test_str(self) -> None:
+        """Test that the Chapter class returns the correct string."""
+        chapter = Chapter(1, "Introduction", "Introducción", [])
+        self.assertEqual(str(chapter), "Chapter 1")
+
+    def test_repr(self) -> None:
+        """Test that the Chapter class returns the correct representation."""	
+        chapter = Chapter(1, "Introduction", "Introducción", [])
+        self.assertEqual(repr(chapter), "Chapter 1")
+
+    def test_eq(self) -> None:
+        """Test that the Chapter class returns the correct equality."""
+        chapter1 = Chapter(1, "Introduction", "Introducción", [])
+        chapter2 = Chapter(1, "Different", "Diferente", [])
+        self.assertEqual(chapter1, chapter2)
+
+    def test_ne(self) -> None:
+        """Test that the Chapter class returns the correct inequality."""
+        chapter1 = Chapter(1, "Introduction", "Introducción", [])
+        chapter2 = Chapter(2, "Different", "Diferente", [])
+        self.assertNotEqual(chapter1, chapter2)
+
+    def test_lt(self) -> None:
+        """Test that the Chapter class returns the correct less than."""
+        chapter1 = Chapter(1, "Introduction", "Introducción", [])
+        chapter2 = Chapter(2, "Different", "Diferente", [])
+        self.assertLess(chapter1, chapter2)
+
+    def test_le(self) -> None:
+        """Test that the Chapter class returns the correct less than or equal."""
+        chapter1 = Chapter(1, "Introduction", "Introducción", [])
+        chapter2 = Chapter(1, "Different", "Diferente", [])
+        self.assertLessEqual(chapter1, chapter2)
+
+    def test_gt(self) -> None:
+        """Test that the Chapter class returns the correct greater than."""
+        chapter1 = Chapter(2, "Different", "Diferente", [])
+        chapter2 = Chapter(1, "Introduction", "Introducción", [])
+        self.assertGreater(chapter1, chapter2)
+
+    def test_ge(self) -> None:
+        """Test that the Chapter class returns the correct greater than or equal."""
+        chapter1 = Chapter(1, "Introduction", "Introducción", [])
+        chapter2 = Chapter(1, "Different", "Diferente", [])
+        self.assertGreaterEqual(chapter1, chapter2)
+
+
+if __name__ == "__main__":
+    unittest.main()
